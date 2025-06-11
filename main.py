5aab28a912a7a8041a535223e25b6dc434c51399
Jonathan Sauter <jsauter@SAUTER2023.localdomain>
2025-06-10 21:57:51 -0600

    create screen

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-background = [1, 1, 2, 2, 2, 1]
-screen = [0]*len(background)
-for i in range(len(background)):
-    screen[i] = background[i]
-print(screen)
-playerpos = 3
-screen[playerpos] = 8
-print(screen)
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((800, 800))
+clock = pygame.time.Clock()
+
+running = True
+dt = 0
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    
+    screen.fill("blue")
+
+    pygame.display.flip()
+
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
+
+
+
+
+
+
